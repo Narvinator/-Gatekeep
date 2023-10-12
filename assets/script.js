ba46e09ae4dff9af70ae2bb21ce6b8f3a38467c4
Roesn <Bubwub04@icloud.com>
2023-10-12 04:31:58 -0400

    init and parse func

--- assets/script.js
+++ assets/script.js
@@ -18,24 +18,61 @@ var client_secret = "65c09d5e68ca4d0a8439a607c8b7e44c";
 
 // const for first part of auth url
 const authorize_url = "https://accounts.spotify.com/authorize";
+const token_url = "https://accounts.spotify.com/api/token";
+
+var redirectURL = "";
 
 // request auth func
 function requestAuthorization(){
 
     console.log("clickkkk");
 
-    // store for later
-    localStorage.setItem("client_id", client_id);
-    localStorage.getItem("client_secret", client_secret);
-
     // build url (add on parameters)
     let url = authorize_url;
-    url += "?client_id=" + client_id;
-    url += "&response_type=code";
-    url += "&redirect_uri=" + encodeURI(redirect_URI);
-    url += "&show_dialog=true";
-    url += "&scope=user-top-read";
+    url += `?client_id=${client_id}&response_type=code&redirect_uri=${encodeURI(redirect_URI)}&show_dialog=true&scope=user-top-read`
 
     // assign ref link to url
     window.location.href = url;
 }
+
+// init func
+function init() {
+
+    // parse redirect url to get returned code 
+    var code = parseRedirect();
+
+    // set up queries for get req 
+    let url = `grant_type=authorization_code&code=${code}&redirect_uri=${encodeURI(redirect_URI)}`
+    url += `&client_id=${client_id}client_secret=${client_secret}`
+
+    // put all tog in req 
+    getTokenRequest(url);
+}
+
+// func to parse redirect url and get code 
+function parseRedirect() {
+    
+    // declare code var
+    var code = null;
+
+    // sett url to search bar 
+    let url = window.location.search;
+
+    // make sure theres queries 
+    if(url.length > 0) {
+
+        // url search var 
+        let query = new URLSearchParams(url);
+
+        // set code to code value from params
+        code = query.get("code");
+    }
+
+    // debug log
+    console.log(code);
+
+    // return code 
+    return code;
+}
+
+
