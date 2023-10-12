c885056f9c494e75157848cb5562fbb837014763
Roesn <Bubwub04@icloud.com>
2023-10-11 22:49:44 -0400

    added jquery lib and click functionality

--- assets/script.js
+++ assets/script.js
@@ -2,10 +2,12 @@
 const burgerIcon = document.querySelector("#burger");
 const navbarMenu = document.querySelector("#nav-links");
 
-burgerIcon.addEventListener('click', (event) => {
-    navbarMenu.classList.toggle("is-active");
-    event.preventDefault();
-});
+// burgerIcon.addEventListener('click', (event) => {
+//     navbarMenu.classList.toggle("is-active");
+//     event.preventDefault();
+// });
+
+$("#sign-in-button").on("click", requestAuthorization);
 
 // redirect uri
 var redirect_URI = "http://127.0.0.1:5500/index.html";
@@ -14,3 +16,11 @@ var redirect_URI = "http://127.0.0.1:5500/index.html";
 var client_id = "a6e8b54904ef4eba84907002452c0ba0";
 var client_secret = "65c09d5e68ca4d0a8439a607c8b7e44c";
 
+// const for first part of auth url
+const authorize_url = "https://accounts.spotify.com/authorize";
+
+// request auth func
+function requestAuthorization(){
+
+    console.log("clickkkk");
+}
