675abd36fba16af1cf1b6d3aa683c8208e8a49b6
Roesn <Bubwub04@icloud.com>
2023-10-11 22:37:46 -0400

    redirect uri and client ids for req

--- assets/script.js
+++ assets/script.js
@@ -7,4 +7,10 @@ burgerIcon.addEventListener('click', (event) => {
     event.preventDefault();
 });
 
+// redirect uri
+var redirect_URI = "http://127.0.0.1:5500/index.html";
+
+// string var for req 
+var client_id = "a6e8b54904ef4eba84907002452c0ba0";
+var client_secret = "65c09d5e68ca4d0a8439a607c8b7e44c";
 
